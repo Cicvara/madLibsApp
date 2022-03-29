8eb91dfa1de7a5774198078f8dc271d4242bbac6
markor <racamarkoo.mr@gmail.com>
2022-03-29 19:24:36 +0200

    Added for loop

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let userInputs = [];
 let originalStory = `One day, ${userInputs[0]} and ${userInputs[1]} decided to take a trip to the beach. ${userInputs[0]} packed a bag full of ${userInputs[2]} and a big jug of ${userInputs[3]}. ${userInputs[1]} brought a towel that had a picture of a ${userInputs[4]} on it. The beach was crowded but there were few spots left. So they put down their towels and enjoyed the sunny day.`;
 let numberOfQuestions = 5;
+let questionArray = ['Enter your name', 'Enter your friends name', 'Enter your favorite food', 'Enter your favorite drink', 'Enter your favorite animal'];
 
-console.log(originalStory);
+for (let i = numberOfQuestions; i>=0; i--) {
+    console.log(i);
+}
+
+
 
 
 
