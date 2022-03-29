ceb33287a2e23e32c2ca598bcb68acd4377aee33
markor <racamarkoo.mr@gmail.com>
2022-03-29 19:09:55 +0200

    Added mad-lib replacement variables to the story

---
+++ app.js
@@ -0,0 +1,19 @@
+let userInputs = [];
+let originalStory = `One day, ${userInputs[0]} and ${userInputs[1]} decided to take a trip to the beach. ${userInputs[0]} packed a bag full of ${userInputs[2]} and a big jug of ${userInputs[3]}. ${userInputs[1]} brought a towel that had a picture of a ${userInputs[4]} on it. The beach was crowded but there were few spots left. So they put down their towels and enjoyed the sunny day.`;
+let numberOfQuestions = 5;
+
+console.log(originalStory);
+
+
+
+
+
+
+
+
+
+
+
+
+// original story
+// One day, - and - decided to take a trip to the beach. - packed a bag full of - and a big jug of -. - brought a towel that had a picture of a - on it. The beach was crowded but there were few spots left.
